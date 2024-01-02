235fd505415431cdffded26abd744bf6468136e3
Jairo Junior <jairojunior.png@gmail.com>
2024-01-02 17:56:26 -0300

    install @fastify/cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import {fastify} from "fastify";
+import { fastifyCors } from "@fastify/cors"
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
 import { generateCompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify()
+app.register(fastifyCors, {
+  origin: "*"
+})
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
