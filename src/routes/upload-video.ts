fbc3624beebb6a1f70a69041d55711c496210862
Jairo Junior <jairojunior.png@gmail.com>
2024-01-02 12:33:06 -0300

    Criando e testando a rota de de upload

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -31,6 +31,14 @@ export async function uploadVideoRoute(app:FastifyInstance) {
     
     await pump(data.file, fs.createWriteStream(uploadDestination))
 
-    reply.send()
+    const video = await prisma.video.create({
+      data: {
+        name: data.filename,
+        path: uploadDestination
+      }
+    })
+    return {
+      video
+    }
   })
 }
