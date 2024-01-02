9a922d5a353a7eb0f06bdf1a44855d9c5f1f498a
Jairo Junior <jairojunior.png@gmail.com>
2024-01-02 17:52:51 -0300

    Criando a rota generate completion

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import {fastify} from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateCompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify()
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateCompletionRoute)
 
 app.listen({
   port: 3333,
