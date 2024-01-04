daf5a158c5c8cd488197b1f589f3d7123232d339
Jairo Junior <jairojunior.png@gmail.com>
2024-01-04 18:54:02 -0300

    rename template to prompt

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -8,10 +8,10 @@ export async function generateCompletionRoute(app:FastifyInstance) {
   app.post('/ai/complete', async (req, reply) => {
     const bodySchema = z.object({
       videoId : z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
     })
-    const { videoId, template, temperature } = bodySchema.parse(req.body)
+    const { videoId, prompt, temperature } = bodySchema.parse(req.body)
     const video = await prisma.video.findUniqueOrThrow({
       where:{
         id : videoId,
@@ -22,7 +22,7 @@ export async function generateCompletionRoute(app:FastifyInstance) {
       return reply.status(400).send({error: "The video transcription was not generated yet"})
     }
 
-    const promptMessage = template.replace('{transcription}', video.transcription)
+    const promptMessage = prompt.replace('{transcription}', video.transcription)
     const response = await openai.chat.completions.create({
       model:"gpt-3.5-turbo-16k",
       temperature,
